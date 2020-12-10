a82d67a22bd686bc58769956a68602a70ce24267
chriswatia <cwatia@seaburysolutions.com>
2020-12-10 21:30:36 +0300

    Created Auth Models

---
+++ Planner.Shared/Models/UserManagerResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Planner.Shared.Models
+{
+    public class UserManagerResponse
+    {
+        public string Message { get; set; }
+        public bool IsSuccess { get; set; }
+        public string[] Errors { get; set; }
+        public DateTime? ExpiryDate { get; set; }
+    }
+}
