bdd0694e780c9f879450079f3fd14bff30973ef5
chriswatia <cwatia@seaburysolutions.com>
2020-12-11 23:08:01 +0300

    send register request to api

---
+++ Planner.Shared/Services/AuthenticationService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AKSoftware.WebApi.Client;
+using Planner.Shared.Models;
+
+namespace Planner.Shared.Services
+{
+    public class AuthenticationService
+    {
+        private readonly string _baseUrl;
+        ServiceClient client = new ServiceClient();
+
+        public AuthenticationService(string url)
+        {
+            _baseUrl = url;
+        }
+
+        public async Task<UserManagerResponse> RegisterUserAsync(RegisterRequest request)
+        {
+            var response = await client.PostAsync<UserManagerResponse>($"{_baseUrl}/api/auth/register", request);
+            return response.Result;
+        }
+    }
+}
